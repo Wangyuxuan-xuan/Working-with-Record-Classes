4852a120be8b8445b7c1528e1abb59e75032fd71
Wangyuxuan-xuan <you@example.com>
2021-02-27 14:56:30 +0100

    fixed the record class

--- src/main/java/geolocator/GeoLocation.java
+++ src/main/java/geolocator/GeoLocation.java
@@ -1,18 +1,5 @@
 package geolocator;
 
-public record GeoLocation() {
-    private static String as;
-    private static String city;
-    private static String country;
-    private static String countryCode;
-    private static String isp;
-    private static double lat;
-    private static double lon;
-    private static String org;
-    private static String query;
-    private static String region;
-    private static String regionName;
-    private static String timezone;
-    private static String zip;
+record GeoLocation(String as,String city,String country,String countryCode,String isp,double lat,double lon,String org,String query,String region,String regionName,String timezone,String zip) {
 
 }
